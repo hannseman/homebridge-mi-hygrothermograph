79038bbf54464715876c4701ef642753fb829e10
Hannes Ljungberg <hannes@5monkeys.se>
2018-03-20 21:18:50 +0100

    Rewrite parser

--- test/test-parser.js
+++ test/test-parser.js
@@ -1,70 +1,80 @@
 const assert = require('assert');
-const { describe, it, xit } = require('mocha');
-const { parseServiceData } = require('../lib/parser');
+const { describe, it } = require('mocha');
+const { Parser } = require('../lib/parser');
 
 describe('parser', () => {
-  xit('should parse flags', () => {
-    const { result } = parseServiceData(Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex'));
-    assert.deepEqual(result.flags, {
-      newFactory: 0,
-      connected: 0,
-      central: 0,
-      encrypted: 0,
-      macAddress: 1,
-      capability: 0,
-      event: 1,
-      customData: 0,
-      subtitle: 0,
-      binding: 0,
+  it('should parse frame control', () => {
+    const buffer = Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex');
+    const result = new Parser(buffer).parse();
+    assert.deepEqual(result.frameControl, {
+      isFactoryNew: false,
+      isConnected: false,
+      isCentral: false,
+      isEncrypted: false,
+      hasMacAddress: true,
+      hasCapabilities: true,
+      hasEvent: true,
+      hasCustomData: false,
+      hasSubtitle: false,
+      hasBinding: false,
     });
   });
 
-  it('should parse id', () => {
-    const { result } = parseServiceData(Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex'));
-    assert.equal(result.id, 426);
+  it('should parse product id', () => {
+    const buffer = Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex');
+    const result = new Parser(buffer).parse();
+
+    assert.equal(result.productId, 426);
   });
 
-  it('should parse index', () => {
-    const { result } = parseServiceData(Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex'));
-    assert.equal(result.index, 176);
+  it('should parse frame counter', () => {
+    const buffer = Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex');
+    const result = new Parser(buffer).parse();
+    assert.equal(result.frameCounter, 176);
   });
 
   it('should parse macAddress', () => {
-    const { result } = parseServiceData(Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex'));
+    const buffer = Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex');
+    const result = new Parser(buffer).parse();
     assert.equal(result.macAddress, '64aed0a865');
   });
 
-  it('should parse capability', () => {
-    const { result } = parseServiceData(Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex'));
-    assert.deepEqual(result.capability, {
+  it('should parse capabilities', () => {
+    const buffer = Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex');
+    const result = new Parser(buffer).parse();
+    assert.deepEqual(result.capabilities, {
       connectable: 0,
       central: 0,
-      encrypt: 1,
-      io: 1,
+      secure: 1,
     });
   });
 
   it('should parse humidity and temperature data', () => {
-    const { result } = parseServiceData(Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex'));
+    const buffer = Buffer.from('5020aa01b064aed0a8654c0d1004d9006001', 'hex');
+    const result = new Parser(buffer).parse();
     assert.equal(result.eventType, 4109);
     assert.equal(result.event.temperature, 21.7);
     assert.equal(result.event.humidity, 35.2);
   });
 
   it('should parse temperature data', () => {
-    const { result } = parseServiceData(Buffer.from('5020aa01a664aed0a8654c041002d900', 'hex'));
+    const buffer = Buffer.from('5020aa01a664aed0a8654c041002d900', 'hex');
+    const result = new Parser(buffer).parse();
     assert.equal(result.eventType, 4100);
     assert.equal(result.event.temperature, 21.7);
   });
 
   it('should parse humidity data', () => {
-    const { result } = parseServiceData(Buffer.from('5020aa01a164aed0a8654c0610025d01', 'hex'));
+    const buffer = Buffer.from('5020aa01a164aed0a8654c0610025d01', 'hex');
+    const result = new Parser(buffer).parse();
     assert.equal(result.eventType, 4102);
     assert.equal(result.event.humidity, 34.9);
   });
 
   it('should parse battery data', () => {
-    const { result } = parseServiceData(Buffer.from('5020aa014e65aed0a8654c0a10015d', 'hex'));
+    const buffer = Buffer.from('5020aa014e65aed0a8654c0a10015d', 'hex');
+    const result = new Parser(buffer).parse();
+
     assert.equal(result.eventType, 4106);
     assert.equal(result.event.battery, 93);
   });

7840ccca9df07e2be795c6fd2058ffee40e18f4d
Hannes Ljungberg <hannes@5monkeys.se>
2018-03-26 17:26:44 +0200

    Extend parser tests

--- test/test-parser.js
+++ test/test-parser.js
@@ -30,34 +30,34 @@ describe('parser', () => {
   Object.keys(sensorData).forEach((sensorKey) => {
     it('should parse product id', () => {
       const result = new Parser(sensorData[sensorKey]).parse();
-      assert.equal(result.productId, 426);
+      assert.strictEqual(result.productId, 426);
     });
   });
 
   it('should parse frame counter', () => {
     const result = new Parser(sensorData.temperatureAndHumidity).parse();
-    assert.equal(result.frameCounter, 176);
+    assert.strictEqual(result.frameCounter, 176);
   });
 
   it('should parse frame counter', () => {
     const result = new Parser(sensorData.temperature).parse();
-    assert.equal(result.frameCounter, 166);
+    assert.strictEqual(result.frameCounter, 166);
   });
 
   it('should parse frame counter', () => {
     const result = new Parser(sensorData.humidity).parse();
-    assert.equal(result.frameCounter, 161);
+    assert.strictEqual(result.frameCounter, 161);
   });
 
   it('should parse frame counter', () => {
     const result = new Parser(sensorData.battery).parse();
-    assert.equal(result.frameCounter, 78);
+    assert.strictEqual(result.frameCounter, 78);
   });
 
   Object.keys(sensorData).forEach((sensorKey) => {
     it('should parse macAddress', () => {
       const result = new Parser(sensorData[sensorKey]).parse();
-      assert.equal(result.macAddress, '64aed0a865');
+      assert.strictEqual(result.macAddress, '64aed0a865');
     });
   });
 
@@ -74,34 +74,34 @@ describe('parser', () => {
 
   it('should parse humidity and temperature data', () => {
     const result = new Parser(sensorData.temperatureAndHumidity).parse();
-    assert.equal(result.eventType, 4109);
-    assert.equal(result.eventLength, 4);
-    assert.equal(result.event.temperature, 21.7);
-    assert.equal(result.event.humidity, 35.2);
+    assert.strictEqual(result.eventType, 4109);
+    assert.strictEqual(result.eventLength, 4);
+    assert.strictEqual(result.event.temperature, 21.7);
+    assert.strictEqual(result.event.humidity, 35.2);
   });
 
   it('should parse temperature data', () => {
     const buffer = Buffer.from(sensorData.temperature, 'hex');
     const result = new Parser(buffer).parse();
-    assert.equal(result.eventType, 4100);
-    assert.equal(result.eventLength, 2);
-    assert.equal(result.event.temperature, 21.7);
+    assert.strictEqual(result.eventType, 4100);
+    assert.strictEqual(result.eventLength, 2);
+    assert.strictEqual(result.event.temperature, 21.7);
   });
 
   it('should parse humidity data', () => {
     const buffer = Buffer.from(sensorData.humidity, 'hex');
     const result = new Parser(buffer).parse();
-    assert.equal(result.eventType, 4102);
-    assert.equal(result.eventLength, 2);
-    assert.equal(result.event.humidity, 34.9);
+    assert.strictEqual(result.eventType, 4102);
+    assert.strictEqual(result.eventLength, 2);
+    assert.strictEqual(result.event.humidity, 34.9);
   });
 
   it('should parse battery data', () => {
     const buffer = Buffer.from(sensorData.battery, 'hex');
     const result = new Parser(buffer).parse();
-    assert.equal(result.eventType, 4106);
-    assert.equal(result.eventLength, 1);
-    assert.equal(result.event.battery, 93);
+    assert.strictEqual(result.eventType, 4106);
+    assert.strictEqual(result.eventLength, 1);
+    assert.strictEqual(result.event.battery, 93);
   });
 
   it('should parse fail on too short', () => {
@@ -117,8 +117,73 @@ describe('parser', () => {
   it('should parse negative temperature', () => {
     const buffer = Buffer.from('5020aa01a664aed0a8654c04100285FF', 'hex');
     const result = new Parser(buffer).parse();
-    assert.equal(result.eventType, 4100);
-    assert.equal(result.eventLength, 2);
-    assert.equal(result.event.temperature, -12.3);
+    assert.strictEqual(result.eventType, 4100);
+    assert.strictEqual(result.eventLength, 2);
+    assert.strictEqual(result.event.temperature, -12.3);
+  });
+
+  it('should handle missing capabilities', () => {
+    const buffer = Buffer.from('5010aa01a664aed0a865041002d900', 'hex');
+    const result = new Parser(buffer).parse();
+    assert.deepEqual(result.frameControl, {
+      isFactoryNew: false,
+      isConnected: false,
+      isCentral: false,
+      isEncrypted: false,
+      hasMacAddress: true,
+      hasCapabilities: false,
+      hasEvent: true,
+      hasCustomData: false,
+      hasSubtitle: false,
+      hasBinding: false,
+    });
+    assert.strictEqual(result.capabilities, null);
+    assert.strictEqual(result.macAddress, '64aed0a865');
+    assert.strictEqual(result.eventType, 4100);
+  });
+
+  it('should handle missing mac address', () => {
+    const buffer = Buffer.from('4020aa01a64c041002d900', 'hex');
+    const result = new Parser(buffer).parse();
+    assert.deepEqual(result.frameControl, {
+      isFactoryNew: false,
+      isConnected: false,
+      isCentral: false,
+      isEncrypted: false,
+      hasMacAddress: false,
+      hasCapabilities: true,
+      hasEvent: true,
+      hasCustomData: false,
+      hasSubtitle: false,
+      hasBinding: false,
+    });
+    assert.strictEqual(result.macAddress, null);
+    assert.deepEqual(result.capabilities, {
+      connectable: 0,
+      central: 0,
+      secure: 1,
+    });
+    assert.strictEqual(result.eventLength, 2);
+    assert.strictEqual(result.eventType, 4100);
+  });
+
+  it('should handle missing event', () => {
+    const buffer = Buffer.from('1020aa01a664aed0a8654c04', 'hex');
+    const result = new Parser(buffer).parse();
+    assert.deepEqual(result.frameControl, {
+      isFactoryNew: false,
+      isConnected: false,
+      isCentral: false,
+      isEncrypted: false,
+      hasMacAddress: true,
+      hasCapabilities: true,
+      hasEvent: false,
+      hasCustomData: false,
+      hasSubtitle: false,
+      hasBinding: false,
+    });
+    assert.strictEqual(result.eventType, null);
+    assert.strictEqual(result.eventLength, null);
+    assert.strictEqual(result.event, null);
   });
 });

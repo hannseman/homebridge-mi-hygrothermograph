9344ff0b7cbe490c3a40488fd267c9130a860eae
Hannes Ljungberg <hannes@5monkeys.se>
2018-04-04 12:51:46 +0200

    Test index.js

---
+++ test/test-index.js
@@ -0,0 +1,23 @@
+const proxyquire = require('proxyquire').noCallThru();
+const assert = require('assert');
+const { describe, it } = require('mocha');
+const sinon = require('sinon');
+
+
+describe('index', () => {
+  it('should export accessory', () => {
+    const registerStub = sinon.stub();
+    const accessoryStub = sinon.stub();
+    const HomebridgeMock = {
+      registerAccessory: registerStub,
+    };
+    proxyquire('../index', {
+      './lib/accessory': () => ({ HygrothermographAccessory: accessoryStub }),
+    })(HomebridgeMock);
+    assert(registerStub.calledWith(
+      'homebridge-mi-hygrothermograph',
+      'Hygrotermograph',
+      accessoryStub
+    ));
+  });
+});

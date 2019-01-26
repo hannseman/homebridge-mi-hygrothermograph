4baaf96bb16e90b851df45ef264776f153ba2012
Gorka Magaña <gorka@gorkamm.com>
2019-01-26 21:16:47 +0100

    Lint errors fixed

--- test/accessory.test.js
+++ test/accessory.test.js
@@ -12,7 +12,7 @@ const {
 } = require("./mocks");
 
 const { Scanner } = proxyquire("../lib/scanner", {
-  '@abandonware/noble': nobleMock
+  "@abandonware/noble": nobleMock
 });
 
 describe("accessory", () => {

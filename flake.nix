1f902c70d3e842f4cd895f78a01e457ee11ffbe4
Hannes Ljungberg <hannes.ljungberg@gmail.com>
2023-12-27 12:14:56 +0100

    Bump dependencies

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; } {
+          packages = [ pkgs.nodejs_20 ];
+        };
+      });
+}
